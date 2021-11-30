6b1b39ba7f2d1915c7ff50af0d8c29340867e8fa
jaguarcion <amokrecov@mail.ru>
2021-11-30 17:09:31 +0700

    optimization

--- src/index.js
+++ src/index.js
@@ -3,21 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Timer extends React.Component{
+
     constructor(props) {
         super(props);
-        const newYear = Date.parse("2021-12-31");
-        const toDay = Date.now();
-        const totalSeconds = (newYear - toDay) / 1000;
-        let seconds = Math.floor(totalSeconds % 60);
-        let minutes = Math.floor((totalSeconds / 60) % 60);
-        let hours = Math.floor((totalSeconds / 3600) % 24);
-        let days = Math.floor(totalSeconds / 3600 / 24);
-
         this.state = {
-            seconds: seconds,
-            minutes: minutes,
-            hours: hours,
-            days: days,
+            seconds: null,
+            minutes: null,
+            hours: null,
+            days: null,
         };
     }
 
@@ -63,27 +56,23 @@ class Timer extends React.Component{
     startInterval(){
 
         let delay = 1000;
+        const newYear = Date.parse("2021-12-31");
+        const toDay = Date.now();
+        const totalSeconds = (newYear - toDay) / 1000;
+        
         this.intervalID = setInterval(()=>{
-            let newSeconds = this.state.seconds - 1;
-           this.setState({seconds: newSeconds});
-            if (this.state.seconds === 0){
-                this.setState({
-                    seconds: 60,
-                    minutes: this.state.minutes -1
-                });
-            }
-            if (this.state.minutes === 0){
-                this.setState({
-                    minutes: 60,
-                    hours: this.state.hours -1
-                });
-            }
-            if (this.state.hours === 0){
-                this.setState({
-                    hours: 24,
-                    days: this.state.days -1
-                });
-            }
+            let seconds = Math.floor(totalSeconds % 60);
+            let minutes = Math.floor((totalSeconds / 60) % 60);
+            let hours = Math.floor((totalSeconds / 3600) % 24);
+            let days = Math.floor(totalSeconds / 3600 / 24);
+
+            this.setState({
+                seconds: seconds,
+                minutes: minutes,
+                days: days,
+                hours: hours
+            });
+
         },delay);
     }
 
